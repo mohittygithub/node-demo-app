28f2920e616fdf99bb90be15fbb1155f1c449767
mohit <smartfriendmohit@gmail.com>
2022-11-26 12:45:14 +0530

    bug fix

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "hello world" });
