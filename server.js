0a124b1e348c9c9d0c0621e8fea8d8f48e67568b
mohit <smartfriendmohit@gmail.com>
2022-11-26 11:04:29 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "hello world" });
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
